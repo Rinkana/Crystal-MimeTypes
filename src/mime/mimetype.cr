9f8c47530461b2dc6520dbb806b6fe93ce7d4901
Max Berends <berendsmax@gmail.com>
2016-04-18 20:02:09 +0200

    Updated mimetypes and use a different style of checking

--- src/mime/mimetype.cr
+++ src/mime/mimetype.cr
@@ -1,12 +1,17 @@
+#TODO: JSON not set
 module Mime
   class MimeType
     getter :type
     getter :extensions
     getter :byte_pattern
 
+    def match_pattern?(pattern)
+
+    end
+
     JSON.mapping({
       type: String,
-      extensions: Array(String),
+      extensions: {type: Array(String), default: []},
       byte_pattern: String
     })
   end
