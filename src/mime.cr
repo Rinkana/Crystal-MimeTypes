e94194979bf0a60758cceca7fc9be9fcd822732c
Max Berends <max@maxberends.nl>
2016-04-18 20:56:42 +0200

    Switched from a single pattern per mime to multiple patters per mime

--- src/mime.cr
+++ src/mime.cr
@@ -17,12 +17,11 @@ module Mime
       header_byte = file.read_byte
       #!header_byte.nil? ? (file_header += sprintf("%02X", file.read_byte) + " ") : (break)
 
-      file_header += sprintf("%02X", file.read_byte) + " " if !header_byte.nil?
+      file_header += sprintf("%02X", header_byte) + " " if !header_byte.nil?
       break if header_byte.nil?
     end
-
     #Try to find a type based on the byte_pattern
-    result = index.select {|mime_type| !mime_type.byte_pattern.empty? && file_header.starts_with? mime_type.byte_pattern}
+    result = index.select {|mime_type| mime_type.match_pattern?(file_header) }
     return result.first if result.size == 1 #single result found, return
 
     file_extension = File.extname(file.path)[1..-1] #Get the extension and remove the dot
@@ -53,7 +52,7 @@ module Mime
       mime_types = [] of MimeType
       JSON.parse(type_defs).each do |typedef|
         mime_type = MimeType.from_json(typedef.to_json)
-        header_size = mime_type.byte_pattern.delete(' ').size / 2
+        header_size = mime_type.biggest_pattern.delete(' ').size / 2
 
         @@header_size = header_size if header_size > @@header_size
 
--- src/mime/mimetype.cr
+++ src/mime/mimetype.cr
@@ -3,16 +3,28 @@ module Mime
   class MimeType
     getter :type
     getter :extensions
-    getter :byte_pattern
+    getter :byte_patterns
 
     def match_pattern?(pattern)
+      @byte_patterns.each do |valid_pattern|
+        return true if pattern.starts_with?(valid_pattern)
+      end
 
+      return false
+    end
+
+    def biggest_pattern
+      @byte_patterns.max_by { |pattern| pattern.size }
+    end
+
+    def smallest_pattern
+      @byte_patterns.min_by { |pattern| pattern.size }
     end
 
     JSON.mapping({
       type: String,
-      extensions: {type: Array(String), default: []},
-      byte_pattern: String
+      extensions: {type: Array(String), default: [] of String},
+      byte_patterns: {type: Array(String), default: [] of String}
     })
   end
 end
